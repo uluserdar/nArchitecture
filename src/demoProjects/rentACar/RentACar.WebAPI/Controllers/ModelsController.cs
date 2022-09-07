66544e2136c2fe1737eae7dfd8b82834a15b5ed7
uluserdar <serdar.uludag@hotmail.com>
2022-09-07 23:21:40 +0300

    ModelsController created

---
+++ src/demoProjects/rentACar/RentACar.WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,22 @@
+﻿using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Feaures.Models.Models;
+using RentACar.Application.Feaures.Models.Queries.GetListModel;
+
+namespace RentACar.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest };
+
+            ModelListModel result = await Mediator.Send(getListModelQuery);
+            return Ok(result);
+        }
+    }
+}
