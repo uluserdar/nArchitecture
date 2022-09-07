d6c9e8f5182e1da5ea76566b84d00b32033b7213
uluserdar <serdar.uludag@hotmail.com>
2022-09-07 22:27:36 +0300

    ModelListModel created

---
+++ src/demoProjects/rentACar/RentACar.Application/Feaures/Models/Models/ModelListModel.cs
@@ -0,0 +1,15 @@
+﻿using Core.Persistence.Paging;
+using RentACar.Application.Feaures.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Feaures.Models.Models
+{
+    public class ModelListModel:BasePageableModel
+    {
+        public IList<ModelListDto> Items { get; set; }
+    }
+}
