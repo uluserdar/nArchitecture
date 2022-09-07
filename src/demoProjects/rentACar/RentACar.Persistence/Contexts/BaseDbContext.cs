a3547fce4190c45e05b146de90a8396d67e8f43a
uluserdar <serdar.uludag@hotmail.com>
2022-09-07 22:02:16 +0300

    BaseDbContext OnModelCreating Method edited. Add-migration add-model

--- src/demoProjects/rentACar/RentACar.Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/RentACar.Persistence/Contexts/BaseDbContext.cs
@@ -29,10 +29,25 @@ namespace RentACar.Persistence.Contexts
                 a.ToTable("Brands").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+                a.HasMany(p => p.Models);
+            });
+
+            modelBuilder.Entity<Model>(m =>
+            {
+                m.ToTable("Models").HasKey(k => k.Id);
+                m.Property(p => p.Id).HasColumnName("Id");
+                m.Property(p => p.BrandId).HasColumnName("BrandId");
+                m.Property(p => p.Name).HasColumnName("Name");
+                m.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+                m.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+                m.HasOne(p => p.Brand);
             });
 
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+
+            Model[] modelEntitySeeds = { new(1, 1, "Series 4", 1500, ""), new(2, 1, "Series 3", 1200, ""), new(3, 2, "A 180", 1000, "") };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
         }
 
     }
