20dd167c19e0003e02315af68edd6a19b3df0e85
uluserdar <serdar.uludag@hotmail.com>
2022-09-07 22:36:03 +0300

    GetListModelQuery created

---
+++ src/demoProjects/rentACar/RentACar.Application/Feaures/Models/Queries/GetListModel/GetListModelQuery.cs
@@ -0,0 +1,24 @@
+﻿using Core.Application.Requests;
+using MediatR;
+using RentACar.Application.Feaures.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Feaures.Models.Queries.GetListModel
+{
+    public class GetListModelQuery:IRequest<ModelListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListModelQueryHandler : IRequestHandler<GetListModelQuery, ModelListModel>
+        {
+            public async Task<ModelListModel> Handle(GetListModelQuery request, CancellationToken cancellationToken)
+            {
+
+            }
+        }
+    }
+}
