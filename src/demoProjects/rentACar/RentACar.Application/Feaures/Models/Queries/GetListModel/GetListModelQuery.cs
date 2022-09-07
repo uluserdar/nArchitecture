37b3160a0b5fd6b50bfa28dd265e42b1a8c8a18c
uluserdar <serdar.uludag@hotmail.com>
2022-09-07 22:40:15 +0300

    GetListModelQuery edited

--- src/demoProjects/rentACar/RentACar.Application/Feaures/Models/Queries/GetListModel/GetListModelQuery.cs
+++ src/demoProjects/rentACar/RentACar.Application/Feaures/Models/Queries/GetListModel/GetListModelQuery.cs
@@ -1,6 +1,8 @@
-﻿using Core.Application.Requests;
+﻿using AutoMapper;
+using Core.Application.Requests;
 using MediatR;
 using RentACar.Application.Feaures.Models.Models;
+using RentACar.Application.Services.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,15 @@ namespace RentACar.Application.Feaures.Models.Queries.GetListModel
 
         public class GetListModelQueryHandler : IRequestHandler<GetListModelQuery, ModelListModel>
         {
+            private readonly IMapper _mapper;
+            private readonly IModelRepository _modelRepository;
+
+            public GetListModelQueryHandler(IMapper mapper, IModelRepository modelRepository)
+            {
+                _mapper = mapper;
+                _modelRepository = modelRepository;
+            }
+
             public async Task<ModelListModel> Handle(GetListModelQuery request, CancellationToken cancellationToken)
             {
 
