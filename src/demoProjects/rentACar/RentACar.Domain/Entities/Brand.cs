01b4edfa269400dc03f9c1034c3fa6b42c4483b9
uluserdar <serdar.uludag@hotmail.com>
2022-09-07 21:36:44 +0300

    Model.cs created

--- src/demoProjects/rentACar/RentACar.Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/RentACar.Domain/Entities/Brand.cs
@@ -5,6 +5,7 @@ namespace RentACar.Domain.Entities
     public class Brand:Entity
     {
         public string Name { get; set; }
+        public virtual ICollection<Model> Models { get; set; }
 
         public Brand() { }
         public Brand(int id,string name):this()
